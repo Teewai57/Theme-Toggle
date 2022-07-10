62c595b3049c55599bc1806d3b350a9cc572f2b5
Teewai57 <bobtoyoabasi@gmail.com>
2022-07-10 23:15:17 +0100

    returned data in article js

--- src/Article.js
+++ src/Article.js
@@ -2,7 +2,16 @@ import React from 'react'
 import moment from 'moment'
 
 const Article = ({ title, snippet, date, length }) => {
-  return <h2>article component</h2>
+  return (
+    <article className='post'>
+    <h2>{title}</h2>
+    <div className='post-info'>
+      <span>{moment(date).format('dddd Do, YYYY')}</span>
+      <span>{length} min read</span>
+    </div>
+    <p>{snippet}</p>
+  </article>
+  )
 }
 
 export default Article
