f4562ac2274227b62aef939d8b08f3a6c660b1ef
Teewai57 <bobtoyoabasi@gmail.com>
2022-07-10 22:59:02 +0100

    initialized theme

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import React, { useState, useEffect } from 'react'
 import data from './data'
 import Article from './Article'
 
+
+const getStorageTheme = () => {
+  let theme = 'light-theme';
+  if(localStorage.getItem('theme')) {
+    theme = localStorage.getItem('theme');
+  }
+  return Home
+}
+
 function App() {
+  const [theme, setTheme] = useState(getStorageTheme());
   return <h2>dark mode starter</h2>
 }
 
