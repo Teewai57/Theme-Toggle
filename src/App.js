41c688b061dffb829bbaa0aaaf5954ad31a7aa18
Teewai57 <bobtoyoabasi@gmail.com>
2022-07-18 10:27:34 +0100

    resetting

--- src/App.js
+++ src/App.js
@@ -1,49 +1,13 @@
-import React, { useState, useEffect } from 'react'
-import data from './data'
-import Article from './Article'
+import React, { useState, useEffect } from 'react';
 
-
-const getStorageTheme = () => {
-  let theme = 'light-theme';
-  if(localStorage.getItem('theme')) {
-    theme = localStorage.getItem('theme');
-  }
-  return Home
-}
 
 function App() {
-  const [theme, setTheme] = useState(getStorageTheme());
-  
-  const toggleTheme = () => {
-    if (theme === 'light-theme') {
-      setTheme('dark-theme');
-    } else {
-      setTheme('light-mode');
-    }
-  };
 
-  useEffect(() => {
-    document.documentElement.className = theme;
-    localStorage.setItem('theme', theme);
-  }, [theme]);
-
-    return (
-    <main>
-      <nav>
-        <div className="nav-center">
-          <h1>overreacted</h1>
-          <button className="btn" onClick={toggleTheme}>
-            toggle
-          </button>
-        </div>
-      </nav>
-      <section className="articles">
-        {data.map((item) => {
-          return <Article key={item.id} {...item} />;
-        })}
-      </section>
-    </main>
+  return (
+   <>
+   <h1>Welcome!</h1>
+   </>
   );
 }
 
-export default App
+export default App;
